2ee5263a3564160328a5573ee446324e215ab3e8
sourav mandal <souravmandalm@gmail.com>
2018-06-07 13:29:57 +0530

    Renamed the module form banner_manager to fossee_site_banner

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function deleteBanner() {
-    var id = Drupal.settings.banner_manager.id;
-    var baseUrl = Drupal.settings.banner_manager.base_url;
+    var id = Drupal.settings.fossee_site_banner.id;
+    var baseUrl = Drupal.settings.fossee_site_banner.base_url;
     console.log("the log is "+baseUrl);
     var txt;
     if (confirm("Banner Once Deleted Is Not Recoverable, do you still want to delete it permanently?")) {
         txt = "You pressed OK!";
-        $.get("http://localhost/fossee/fossee_drupal/banner_manager/delete_banner/"+id+'/arg'+Math.random()).success(function(data){
+        $.get("http://localhost/fossee/fossee_drupal/fossee_site_banner/delete_banner/"+id+'/arg'+Math.random()).success(function(data){
 
             console.log("result is "+data.result);
 
             if(data.result === 'success'){
-                window.location = baseUrl+"banner_manager/banners";
+                window.location = baseUrl+"fossee_site_banner/banners";
                 console.log("result success");
             }
 
@@ -28,7 +28,7 @@ function deleteBanner() {
 
 /*
 (function($) {
-    Drupal.settings.banner_manager = {
+    Drupal.settings.fossee_site_banner = {
         attach: function (context, settings) {
             alert(settings.MODULENAME.testvar);
         }
